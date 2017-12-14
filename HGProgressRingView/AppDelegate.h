0c347e2f59105e97fffaee76bc83b35bfaa75f6a
gang gang <huanggang@huanggangde-mac-mini.local>
2017-12-14 10:04:52 +0800

    Initial Commit

---
+++ HGProgressRingView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HGProgressRingView
+//
+//  Created by gang on 2017/12/14.
+//  Copyright © 2017年 gang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HGProgressRingView/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  HGProgressRingView
+//
+//  Created by gang on 2017/12/14.
+//  Copyright © 2017年 gang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
